5de1af4c707c471c9dc5dd1e682b0211e442640a
limasuu <szlima.93@gmail.com>
2022-08-23 17:49:21 -0300

    Gerando os números primos do intervalo.

--- src/Entrada.js
+++ src/Entrada.js
@@ -5,7 +5,7 @@ class Entrada extends React.Component{
 
     render(){
         return (
-            <form className='d-flex'>
+            <form className='d-flex' onSubmit={this.props.funcaoBotao}>
                 <input type='number' required className='form-control' onChange={this.props.funcaoCampo}
                      placeholder='Mínimo' name='minimo' value={this.props.campoMinimo}/>
                 <input type='number' required className='form-control' onChange={this.props.funcaoCampo}
@@ -18,7 +18,8 @@ class Entrada extends React.Component{
 }
 
 Entrada.propTypes= {
-    funcaoCampo: PropTypes.func.isRequired
+    funcaoCampo: PropTypes.func.isRequired,
+    funcaoBotao: PropTypes.func.isRequired
 };
 
 export default Entrada;
--- src/Principal.js
+++ src/Principal.js
@@ -16,13 +16,35 @@ class Principal extends React.Component{
         [campo.target.name]: parseInt(campo.target.value, 10)
     });
 
+    enviar= botao => {
+        botao.preventDefault();
+
+        let primosCopia= [];
+        for(let i=this.state.minimo; i<=this.state.maximo; i++){
+            if(i===2 || (i > 2 && i%2!==0)){
+
+                let dividiu= false;
+                for(let j=3; j<=Math.sqrt(i); j++)
+                    if(i%j===0){
+                        dividiu= true;
+                        break;
+                    }
+                
+                if(!dividiu)
+                    primosCopia.push(i);
+            } 
+        }        
+        
+        console.log(primosCopia);//
+    };
+
     render(){
         return (
             <main>
                 <h1>Números primos</h1>
 
                 <Entrada campoMinimo={this.state.minimo} campoMaximo={this.state.maximo}
-                    funcaoCampo={this.digitar}/>
+                    funcaoCampo={this.digitar} funcaoBotao={this.enviar}/>
             </main>
         );
     }
